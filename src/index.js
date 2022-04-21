1cc3f0bb754bcae89b2d0ae34453a1ce5a4fb5b7
IliaZhukovskii <ilia.zhukovskii.99@mail.ru>
2022-04-21 12:21:11 +0300

    валидация форм

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import timer from './modules/timer';
 import menu from './modules/menu';
 import modal from './modules/modal';
+import validateForm from './modules/validateForm';
 
 
 timer('30 april 2022');
 menu();
 modal();
+validateForm();
